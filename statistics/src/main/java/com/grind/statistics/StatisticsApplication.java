a8eca3001a5eaa00602d2a6cff96dbd997ffd85d
e901st <egorushkinnikita65@gmail.com>
2025-11-24 22:24:21 +0000

    add: set up dependencies and properties

--- statistics/src/main/java/com/grind/statistics/StatisticsApplication.java
+++ statistics/src/main/java/com/grind/statistics/StatisticsApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan({"com.grind.security"})
 public class StatisticsApplication {
 
 	public static void main(String[] args) {
