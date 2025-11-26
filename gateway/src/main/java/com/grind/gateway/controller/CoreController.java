499a6c470324fa19ebd3d79781c220f2e5d923b4
e901st <egorushkinnikita65@gmail.com>
2025-11-26 21:32:56 +0000

    add: created controllers to handle frontend requests

---
+++ gateway/src/main/java/com/grind/gateway/controller/CoreController.java
@@ -0,0 +1,9 @@
+package com.grind.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/core")
+public class CoreController {
+}
