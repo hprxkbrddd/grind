699788c8fd1e79fcac39075c9b5ec63c636cf1a4
Beoblodya <asyrtsov22@gmail.com>
2025-12-03 23:32:23 -1200

    Ignore

--- core/src/main/java/com/grind/core/dto/SprintDTO.java
+++ core/src/main/java/com/grind/core/dto/SprintDTO.java
@@ -1,9 +1,22 @@
 package com.grind.core.dto;
 
+import com.grind.core.model.Track;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public record SprintDTO(
         String id,
 
-        String trackId,
+        String name,
 
-        short duration) {
+        LocalDate startDate,
+
+        LocalDate endDate,
+
+        String status,
+
+        LocalDateTime createdAt,
+
+        Track track) {
 }
