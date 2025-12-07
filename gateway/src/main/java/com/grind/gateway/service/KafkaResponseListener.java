358d047049404d1ba9c12f12c3999c71f02c2697
e901st <egorushkinnikita65@gmail.com>
2025-12-07 13:31:50 +0000

    add: configured response retrieving in api way, but for only one endpoint

--- gateway/src/main/java/com/grind/gateway/service/KafkaResponseListener.java
+++ gateway/src/main/java/com/grind/gateway/service/KafkaResponseListener.java
@@ -2,6 +2,8 @@ package com.grind.gateway.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -9,29 +11,26 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Service
-public class KafkaResponseListener {
-    private final WebClient webClient;
-    private final ObjectMapper objectMapper;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
-    KafkaResponseListener(){
-        this.webClient = WebClient.builder()
-                .baseUrl("http://localhost:3000/frontend/response/uri")
-                .build();
-        this.objectMapper = new ObjectMapper();
-    }
+@Service
+@RequiredArgsConstructor
+public class KafkaResponseListener {
+    private final ObjectMapper objectMapper;
+    private final PendingRegistry pendingRegistry;
 
     @KafkaListener(id = "gateway", topics = "response")
-    public void sendResponse(
+    public void handleResponse(
             @Payload String payload,
             @Header(KafkaHeaders.CORRELATION_ID) String correlationId
     ) throws JsonProcessingException {
         Object response = objectMapper.readValue(payload, Object.class);
-//        webClient.post()
-//                .bodyValue(response)
-//                .header("X-Correlation-Id", correlationId)
-//                .retrieve()
-//                .toBodilessEntity()
-//                .subscribe();
+        CompletableFuture<Object> future = pendingRegistry.remove(correlationId);
+        if (future!=null){
+            future.complete(response);
+        } else {
+            System.out.println(">>>>>> I'M NOT YOUR BRAH");
+        }
     }
 }
